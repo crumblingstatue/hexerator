3215e6a1e842510e292778d776d42b023952ffbe
crumblingstatue <radiantstatue@gmail.com>
2022-05-30 00:34:18 +0200

    Split ascii view into function

--- src/main.rs
+++ src/main.rs
@@ -221,6 +221,12 @@ fn draw_inner(app: &mut App, window: &mut RenderWindow, font: &Font) {
     imm_msg!(rows_rendered);
     cols_rendered = cols_rendered.checked_div(rows_rendered).unwrap_or(0);
     imm_msg!(cols_rendered);
+    if app.show_text {
+        ascii_view(app, view_idx_off_y, window, font);
+    }
+}
+
+fn ascii_view(app: &mut App, view_idx_off_y: usize, window: &mut RenderWindow, font: &Font) {
     // The offset for the ascii display imposed by the view
     let ascii_display_x_offset = app.ascii_display_x_offset();
     imm_msg!(ascii_display_x_offset);
@@ -238,70 +244,62 @@ fn draw_inner(app: &mut App, window: &mut RenderWindow, font: &Font) {
     imm_msg!(view_idx_off);
     let mut ascii_rows_rendered: u32 = 0;
     let mut ascii_cols_rendered: u32 = 0;
-    if app.show_text {
-        idx = app.view.start_offset + view_idx_off;
-        imm_msg!(idx);
-        'asciidisplay: for y in 0..app.view.rows {
-            for x in 0..app.view.cols {
-                if x == app.max_visible_cols * 2
-                    || x >= app.view.cols.saturating_sub(view_idx_off_x)
-                {
-                    idx += app.view.cols - x;
-                    break;
-                }
-                if idx >= app.data.len() {
-                    break 'asciidisplay;
-                }
-                let pix_x = (x + app.view.cols * 2 + 1) as f32 * f32::from(app.col_width / 2)
-                    - app.view_x as f32;
-                //let pix_y = y as f32 * f32::from(app.row_height) - app.view_y as f32;
-                let pix_y =
-                    (y + view_idx_off_y) as f32 * f32::from(app.row_height) - app.view_y as f32;
-                let byte = app.data[idx];
-                let c = byte_color(byte, !app.colorize);
-                let selected = match app.selection {
-                    Some(sel) => (sel.begin..=sel.end).contains(&idx),
-                    None => false,
-                };
-                if selected
-                    || (app.find_dialog.open && app.find_dialog.result_offsets.contains(&idx))
-                {
-                    let mut rs = RectangleShape::from_rect(Rect::new(
-                        pix_x,
-                        pix_y,
-                        (app.col_width / 2) as f32,
-                        app.row_height as f32,
-                    ));
-                    rs.set_fill_color(Color::rgb(150, 150, 150));
-                    if app.cursor == idx {
-                        rs.set_outline_color(Color::WHITE);
-                        rs.set_outline_thickness(-2.0);
-                    }
-                    window.draw(&rs);
-                }
-                if idx == app.cursor {
-                    draw_cursor(
-                        pix_x,
-                        pix_y,
-                        window,
-                        app.edit_target == EditTarget::Text
-                            && app.interact_mode == InteractMode::Edit,
-                    );
-                }
-                draw_glyph(
-                    font,
-                    app.font_size,
-                    &mut app.vertices,
+    let mut idx = app.view.start_offset + view_idx_off;
+    imm_msg!(idx);
+    'asciidisplay: for y in 0..app.view.rows {
+        for x in 0..app.view.cols {
+            if x == app.max_visible_cols * 2 || x >= app.view.cols.saturating_sub(view_idx_off_x) {
+                idx += app.view.cols - x;
+                break;
+            }
+            if idx >= app.data.len() {
+                break 'asciidisplay;
+            }
+            let pix_x = (x + app.view.cols * 2 + 1) as f32 * f32::from(app.col_width / 2)
+                - app.view_x as f32;
+            //let pix_y = y as f32 * f32::from(app.row_height) - app.view_y as f32;
+            let pix_y = (y + view_idx_off_y) as f32 * f32::from(app.row_height) - app.view_y as f32;
+            let byte = app.data[idx];
+            let c = byte_color(byte, !app.colorize);
+            let selected = match app.selection {
+                Some(sel) => (sel.begin..=sel.end).contains(&idx),
+                None => false,
+            };
+            if selected || (app.find_dialog.open && app.find_dialog.result_offsets.contains(&idx)) {
+                let mut rs = RectangleShape::from_rect(Rect::new(
                     pix_x,
                     pix_y,
-                    byte as u32,
-                    c,
-                );
-                idx += 1;
-                ascii_cols_rendered += 1;
+                    (app.col_width / 2) as f32,
+                    app.row_height as f32,
+                ));
+                rs.set_fill_color(Color::rgb(150, 150, 150));
+                if app.cursor == idx {
+                    rs.set_outline_color(Color::WHITE);
+                    rs.set_outline_thickness(-2.0);
+                }
+                window.draw(&rs);
             }
-            ascii_rows_rendered += 1;
+            if idx == app.cursor {
+                draw_cursor(
+                    pix_x,
+                    pix_y,
+                    window,
+                    app.edit_target == EditTarget::Text && app.interact_mode == InteractMode::Edit,
+                );
+            }
+            draw_glyph(
+                font,
+                app.font_size,
+                &mut app.vertices,
+                pix_x,
+                pix_y,
+                byte as u32,
+                c,
+            );
+            idx += 1;
+            ascii_cols_rendered += 1;
         }
+        ascii_rows_rendered += 1;
     }
     imm_msg!(ascii_rows_rendered);
     ascii_cols_rendered = ascii_cols_rendered
