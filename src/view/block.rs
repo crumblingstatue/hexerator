29bf61af36758a44035cca84c3ff08a3707aec43
crumblingstatue <radiantstatue@gmail.com>
2022-07-19 20:55:59 +0200

    Rename "lens" to "view" in rendering code

--- src/view/block.rs
+++ src/view/block.rs
@@ -5,7 +5,7 @@ use crate::{app::App, view::draw_cursor};
 use super::{draw_rect, View};
 
 pub fn block(
-    lens: &View,
+    view: &View,
     app: &mut App,
     _window: &mut RenderWindow,
     vertex_buffer: &mut Vec<Vertex>,
@@ -13,24 +13,24 @@ pub fn block(
     let view_x = 0;
     let view_y = 0;
     let mut idx = app.perspective.region.begin;
-    let start_row = view_y.try_into().unwrap_or(0) / lens.row_h as usize;
+    let start_row = view_y.try_into().unwrap_or(0) / view.row_h as usize;
     idx += start_row * app.perspective.cols;
     'rows: for row in start_row.. {
-        let y = row as f32 * f32::from(lens.row_h);
-        let yy = (lens.viewport_rect.y as f32 + y) - view_y as f32;
-        let start_col = view_x.try_into().unwrap_or(0) / lens.col_w as usize;
+        let y = row as f32 * f32::from(view.row_h);
+        let yy = (view.viewport_rect.y as f32 + y) - view_y as f32;
+        let start_col = view_x.try_into().unwrap_or(0) / view.col_w as usize;
         if start_col >= app.perspective.cols {
             break;
         }
         idx += start_col;
         for col in start_col..app.perspective.cols {
-            let x = col as f32 * f32::from(lens.col_w);
-            let xx = (lens.viewport_rect.x as f32 + x) - view_x as f32;
-            if xx > (lens.viewport_rect.x + lens.viewport_rect.w) as f32 {
+            let x = col as f32 * f32::from(view.col_w);
+            let xx = (view.viewport_rect.x as f32 + x) - view_x as f32;
+            if xx > (view.viewport_rect.x + view.viewport_rect.w) as f32 {
                 idx += app.perspective.cols - col;
                 break;
             }
-            if yy > (lens.viewport_rect.y + lens.viewport_rect.h) as f32 || idx >= app.data.len() {
+            if yy > (view.viewport_rect.y + view.viewport_rect.h) as f32 || idx >= app.data.len() {
                 break 'rows;
             }
             let byte = app.data[idx];
@@ -42,8 +42,8 @@ pub fn block(
                 vertex_buffer,
                 xx,
                 yy,
-                lens.col_w as f32,
-                lens.row_h as f32,
+                view.col_w as f32,
+                view.row_h as f32,
                 c,
             );
             if idx == app.edit_state.cursor {
--- src/view/hex.rs
+++ src/view/hex.rs
@@ -8,28 +8,28 @@ use crate::{
 
 use super::View;
 
-pub fn hex(lens: &View, app: &mut App, font: &Font, vertex_buffer: &mut Vec<Vertex>) {
+pub fn hex(view: &View, app: &mut App, font: &Font, vertex_buffer: &mut Vec<Vertex>) {
     let view_x = 0;
     let view_y = 0;
     let mut idx = app.perspective.region.begin;
-    let start_row = view_y.try_into().unwrap_or(0) / lens.row_h as usize;
+    let start_row = view_y.try_into().unwrap_or(0) / view.row_h as usize;
     idx += start_row * app.perspective.cols;
     'rows: for row in start_row.. {
-        let y = row as f32 * f32::from(lens.row_h);
-        let yy = (lens.viewport_rect.y as f32 + y) - view_y as f32;
-        let start_col = view_x.try_into().unwrap_or(0) / lens.col_w as usize;
+        let y = row as f32 * f32::from(view.row_h);
+        let yy = (view.viewport_rect.y as f32 + y) - view_y as f32;
+        let start_col = view_x.try_into().unwrap_or(0) / view.col_w as usize;
         if start_col >= app.perspective.cols {
             break;
         }
         idx += start_col;
         for col in start_col..app.perspective.cols {
-            let x = col as f32 * f32::from(lens.col_w);
-            let xx = (lens.viewport_rect.x as f32 + x) - view_x as f32;
-            if xx > (lens.viewport_rect.x + lens.viewport_rect.w) as f32 {
+            let x = col as f32 * f32::from(view.col_w);
+            let xx = (view.viewport_rect.x as f32 + x) - view_x as f32;
+            if xx > (view.viewport_rect.x + view.viewport_rect.w) as f32 {
                 idx += app.perspective.cols - col;
                 break;
             }
-            if yy > (lens.viewport_rect.y + lens.viewport_rect.h) as f32 || idx >= app.data.len() {
+            if yy > (view.viewport_rect.y + view.viewport_rect.h) as f32 || idx >= app.data.len() {
                 break 'rows;
             }
             let byte = app.data[idx];
@@ -51,7 +51,7 @@ pub fn hex(lens: &View, app: &mut App, font: &Font, vertex_buffer: &mut Vec<Vert
                 font,
                 app.layout.font_size.into(),
                 vertex_buffer,
-                xx + (lens.col_w / 2) as f32 - 4.0,
+                xx + (view.col_w / 2) as f32 - 4.0,
                 yy,
                 d2.into(),
                 c,
