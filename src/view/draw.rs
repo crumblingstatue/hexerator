adb6ed6f016d59d667c25a9e03cf9018f9586a36
crumblingstatue <radiantstatue@gmail.com>
2022-07-20 18:35:14 +0200

    Rename xx, yy, to viewport_x, viewport_y

--- src/view/draw.rs
+++ src/view/draw.rs
@@ -26,7 +26,7 @@ pub fn draw_view(
     idx += start_row * app.perspective.cols;
     'rows: for row in start_row.. {
         let y = row as f32 * f32::from(view.row_h);
-        let yy = (view.viewport_rect.y as f32 + y) - view_y as f32;
+        let viewport_y = (view.viewport_rect.y as f32 + y) - view_y as f32;
         let start_col = view_x.try_into().unwrap_or(0) / view.col_w as usize;
         if start_col >= app.perspective.cols {
             break;
@@ -34,12 +34,14 @@ pub fn draw_view(
         idx += start_col;
         for col in start_col..app.perspective.cols {
             let x = col as f32 * f32::from(view.col_w);
-            let xx = (view.viewport_rect.x as f32 + x) - view_x as f32;
-            if xx > (view.viewport_rect.x + view.viewport_rect.w) as f32 {
+            let viewport_x = (view.viewport_rect.x as f32 + x) - view_x as f32;
+            if viewport_x > (view.viewport_rect.x + view.viewport_rect.w) as f32 {
                 idx += app.perspective.cols - col;
                 break;
             }
-            if yy > (view.viewport_rect.y + view.viewport_rect.h) as f32 || idx >= app.data.len() {
+            if viewport_y > (view.viewport_rect.y + view.viewport_rect.h) as f32
+                || idx >= app.data.len()
+            {
                 break 'rows;
             }
             let byte = app.data[idx];
@@ -47,11 +49,11 @@ pub fn draw_view(
                 .presentation
                 .color_method
                 .byte_color(byte, app.presentation.invert_color);
-            drawfn(vertex_buffer, xx, yy, byte, c);
+            drawfn(vertex_buffer, viewport_x, viewport_y, byte, c);
             if idx == app.edit_state.cursor {
                 draw_cursor(
-                    xx,
-                    yy,
+                    viewport_x,
+                    viewport_y,
                     vertex_buffer,
                     true,
                     app.cursor_flash_timer(),
