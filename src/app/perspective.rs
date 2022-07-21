2e5363fa73971e5da9cb639bc5cf3aaab837a85e
crumblingstatue <radiantstatue@gmail.com>
2022-07-21 22:32:40 +0200

    perspective: Fix byte offset calculation when begin isn't 0

--- src/app/perspective.rs
+++ src/app/perspective.rs
@@ -16,7 +16,7 @@ impl Perspective {
         self.region.end / self.cols
     }
     pub(crate) fn byte_offset_of_row_col(&self, row: usize, col: usize) -> usize {
-        row * self.cols + col
+        self.region.begin + (row * self.cols + col)
     }
     pub(crate) fn row_col_of_byte_offset(&self, offset: usize) -> (usize, usize) {
         (offset / self.cols, offset % self.cols)
