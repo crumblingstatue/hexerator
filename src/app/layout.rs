b4c3030d656f75b3f9ea82f2707a334f468f7d5f
crumblingstatue <radiantstatue@gmail.com>
2022-07-23 18:03:19 +0200

    Remove block_size from Layout struct

--- src/app/layout.rs
+++ src/app/layout.rs
@@ -8,15 +8,12 @@ pub struct Layout {
     pub max_visible_cols: usize,
     /// Font size
     pub font_size: u8,
-    /// Block size for block view
-    pub block_size: u8,
 }
 
 impl Layout {
     pub fn new(window_height: u32) -> Self {
         Self {
             font_size: 14,
-            block_size: 4,
             max_visible_cols: 75,
             top_gap: 46,
             bottom_gap: 25,
