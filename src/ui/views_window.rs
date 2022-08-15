66ed1034635cae7cac1e66609b42d2f06fb8f609
crumblingstatue <radiantstatue@gmail.com>
2022-08-15 16:20:45 +0200

    Make views window ui more compact, edit 1 item at a time

--- src/ui/views_window.rs
+++ src/ui/views_window.rs
@@ -11,6 +11,7 @@ use crate::{
 #[derive(Debug)]
 pub struct ViewsWindow {
     pub open: bool,
+    pub selected: usize,
     new_kind: ViewKind,
 }
 
@@ -19,6 +20,7 @@ impl Default for ViewsWindow {
         Self {
             open: Default::default(),
             new_kind: ViewKind::Hex(HexData::default()),
+            selected: 0,
         }
     }
 }
@@ -43,103 +45,111 @@ pub const MAX_FONT_SIZE: u16 = 256;
 
 impl ViewsWindow {
     pub(crate) fn ui(ui: &mut egui_sfml::egui::Ui, app: &mut crate::app::App, font: &Font) {
-        let mut idx = 0;
         let mut removed_idx = None;
-        app.named_views.retain_mut(|view| {
-            let mut retain = true;
-            ui.group(|ui| {
-                ui.horizontal(|ui| {
-                    ui.label("Name");
-                    ui.text_edit_singleline(&mut view.name);
-                });
-                if view_combo(
-                    egui::Id::new("view_combo").with(idx),
-                    &mut view.view.kind,
-                    ui,
-                    font,
-                ) {
-                    view.view.adjust_state_to_kind();
+        for (i, view) in app.named_views.iter().enumerate() {
+            ui.horizontal(|ui| {
+                if ui
+                    .selectable_label(i == app.ui.views_window.selected, &view.name)
+                    .clicked()
+                {
+                    app.ui.views_window.selected = i;
                 }
-                let mut adjust_block_size = false;
-                match &mut view.view.kind {
-                    ViewKind::Hex(HexData { font_size, .. })
-                    | ViewKind::Dec(HexData { font_size, .. })
-                    | ViewKind::Text(TextData { font_size, .. }) => {
-                        ui.horizontal(|ui| {
-                            ui.label("Font size");
-                            if ui
-                                .add(
-                                    egui::DragValue::new(font_size)
-                                        .clamp_range(MIN_FONT_SIZE..=MAX_FONT_SIZE),
-                                )
-                                .changed()
-                            {
-                                adjust_block_size = true;
-                            };
-                        });
-                        if let ViewKind::Text(text) = &mut view.view.kind {
-                            let mut changed = false;
-                            egui::ComboBox::new(egui::Id::new("text_combo").with(idx), "Text kind")
-                                .selected_text(text.text_kind.name())
-                                .show_ui(ui, |ui| {
-                                    changed |= ui
-                                        .selectable_value(
-                                            &mut text.text_kind,
-                                            TextKind::Ascii,
-                                            TextKind::Ascii.name(),
-                                        )
-                                        .clicked();
-                                    changed |= ui
-                                        .selectable_value(
-                                            &mut text.text_kind,
-                                            TextKind::Utf16Le,
-                                            TextKind::Utf16Le.name(),
-                                        )
-                                        .clicked();
-                                    changed |= ui
-                                        .selectable_value(
-                                            &mut text.text_kind,
-                                            TextKind::Utf16Be,
-                                            TextKind::Utf16Be.name(),
-                                        )
-                                        .clicked();
-                                });
-                            if changed {
-                                view.view.bytes_per_block = text.text_kind.bytes_needed();
-                            }
+                ui.label(egui::RichText::new(view.view.kind.name()).code());
+            });
+        }
+        ui.separator();
+        if let Some(view) = app.named_views.get_mut(app.ui.views_window.selected) {
+            ui.horizontal(|ui| {
+                ui.label("Name");
+                ui.text_edit_singleline(&mut view.name);
+            });
+            if view_combo(egui::Id::new("view_combo"), &mut view.view.kind, ui, font) {
+                view.view.adjust_state_to_kind();
+            }
+            let mut adjust_block_size = false;
+            match &mut view.view.kind {
+                ViewKind::Hex(HexData { font_size, .. })
+                | ViewKind::Dec(HexData { font_size, .. })
+                | ViewKind::Text(TextData { font_size, .. }) => {
+                    ui.horizontal(|ui| {
+                        ui.label("Font size");
+                        if ui
+                            .add(
+                                egui::DragValue::new(font_size)
+                                    .clamp_range(MIN_FONT_SIZE..=MAX_FONT_SIZE),
+                            )
+                            .changed()
+                        {
+                            adjust_block_size = true;
+                        };
+                    });
+                    if let ViewKind::Text(text) = &mut view.view.kind {
+                        let mut changed = false;
+                        egui::ComboBox::new(egui::Id::new("text_combo"), "Text kind")
+                            .selected_text(text.text_kind.name())
+                            .show_ui(ui, |ui| {
+                                changed |= ui
+                                    .selectable_value(
+                                        &mut text.text_kind,
+                                        TextKind::Ascii,
+                                        TextKind::Ascii.name(),
+                                    )
+                                    .clicked();
+                                changed |= ui
+                                    .selectable_value(
+                                        &mut text.text_kind,
+                                        TextKind::Utf16Le,
+                                        TextKind::Utf16Le.name(),
+                                    )
+                                    .clicked();
+                                changed |= ui
+                                    .selectable_value(
+                                        &mut text.text_kind,
+                                        TextKind::Utf16Be,
+                                        TextKind::Utf16Be.name(),
+                                    )
+                                    .clicked();
+                            });
+                        if changed {
+                            view.view.bytes_per_block = text.text_kind.bytes_needed();
                         }
                     }
-                    ViewKind::Block => {}
                 }
-                if adjust_block_size {
-                    view.view.adjust_block_size();
-                    #[expect(
-                        clippy::cast_possible_truncation,
-                        clippy::cast_sign_loss,
-                        reason = "It's extremely unlikely line spacing is not between 0 and i16::MAX"
-                    )]
-                    if let ViewKind::Text(data) = &mut view.view.kind {
-                        data.line_spacing = font.line_spacing(u32::from(data.font_size)) as u16;
-                    }
+                ViewKind::Block => {}
+            }
+            if adjust_block_size {
+                view.view.adjust_block_size();
+                #[expect(
+                    clippy::cast_possible_truncation,
+                    clippy::cast_sign_loss,
+                    reason = "It's extremely unlikely line spacing is not between 0 and i16::MAX"
+                )]
+                if let ViewKind::Text(data) = &mut view.view.kind {
+                    data.line_spacing = font.line_spacing(u32::from(data.font_size)) as u16;
                 }
-                viewport_rect_ui(ui, &mut view.view.viewport_rect);
-                labelled_drag(ui, "column width", &mut view.view.col_w, 1..=128);
-                labelled_drag(ui, "row height", &mut view.view.row_h, 1..=128);
-                labelled_drag(ui, "bytes per block", &mut view.view.bytes_per_block, 1..=64);
-                ui.checkbox(&mut view.view.active, "Active");
-                if ui.button("Delete").clicked() {
-                    retain = false;
-                    removed_idx = Some(idx);
+            }
+            viewport_rect_ui(ui, &mut view.view.viewport_rect);
+            labelled_drag(ui, "column width", &mut view.view.col_w, 1..=128);
+            labelled_drag(ui, "row height", &mut view.view.row_h, 1..=128);
+            labelled_drag(
+                ui,
+                "bytes per block",
+                &mut view.view.bytes_per_block,
+                1..=64,
+            );
+            ui.checkbox(&mut view.view.active, "Active");
+            if ui.button("Delete").clicked() {
+                removed_idx = Some(app.ui.views_window.selected);
+            }
+        }
+        if let Some(rem_idx) = removed_idx {
+            app.named_views.remove(rem_idx);
+            if let Some(focused) = &mut app.focused_view && *focused >= rem_idx {
+                if app.named_views.is_empty() {
+                    app.focused_view = None;
+                } else if *focused > 0 {
+                    *focused -= 1;
                 }
-                idx += 1;
-            });
-            retain
-        });
-        if let Some(focused) = &mut app.focused_view && let Some(rem_idx) = removed_idx && *focused >= rem_idx {
-            if app.named_views.is_empty() {
-                app.focused_view = None;
-            } else if *focused > 0 {
-                *focused -= 1;
             }
         }
         ui.separator();
