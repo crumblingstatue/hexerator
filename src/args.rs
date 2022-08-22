b380a195855312852a62c6870476c469d60eeabb
crumblingstatue <radiantstatue@gmail.com>
2022-08-22 20:57:50 +0200

    Allow hexadecimal offsets in command line arguments

--- src/args.rs
+++ src/args.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, str::FromStr};
 
 use clap::Parser;
 use serde::{Deserialize, Serialize};
@@ -8,13 +8,13 @@ pub struct Args {
     /// The file to read
     pub file: Option<PathBuf>,
     /// Jump to offset on startup
-    #[clap(short = 'j')]
+    #[clap(short = 'j', value_parser = parse_offset)]
     pub jump: Option<usize>,
     /// Seek to offset, consider it beginning of the file in the editor
-    #[clap(long)]
+    #[clap(long, value_parser = parse_offset)]
     pub hard_seek: Option<usize>,
     /// Read only this many bytes
-    #[clap(long)]
+    #[clap(long, value_parser = parse_offset)]
     pub take: Option<usize>,
     /// Open file as read-only, without writing privileges
     #[clap(long)]
@@ -29,4 +29,14 @@ pub struct Args {
     /// Load most recently used file
     #[clap(long)]
     pub load_recent: bool,
+}
+
+fn parse_offset(arg: &str) -> Result<usize, <usize as FromStr>::Err> {
+    if let Some(stripped) = arg.strip_prefix("0x") {
+        usize::from_str_radix(stripped, 16)
+    } else if arg.contains(['a', 'b', 'c', 'd', 'e', 'f']) {
+        usize::from_str_radix(arg, 16)
+    } else {
+        arg.parse()
+    }
 }
