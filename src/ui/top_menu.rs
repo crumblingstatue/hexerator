65c7dddd5e8404c329733ab23079a720442d7446
crumblingstatue <radiantstatue@gmail.com>
2022-09-05 15:36:31 +0200

    Top menu: Rename "current layout" to just "layout"

--- src/ui/top_menu.rs
+++ src/ui/top_menu.rs
@@ -202,7 +202,7 @@ pub fn top_menu(ui: &mut egui::Ui, app: &mut App, font: &Font) {
             }
         });
         ui.menu_button("View", |ui| {
-            ui.menu_button("Current layout", |ui| {
+            ui.menu_button("Layout", |ui| {
                 for (k, v) in &app.meta.layouts {
                     if ui.selectable_label(app.current_layout == k, &v.name).clicked() {
                         app.current_layout = k;
