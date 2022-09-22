2af28c18b1301f76982cb3b3823b66d93ef781e4
crumblingstatue <radiantstatue@gmail.com>
2022-09-22 11:00:21 +0200

    About dialog: Display target triple

--- src/gui/windows/about.rs
+++ src/gui/windows/about.rs
@@ -18,10 +18,12 @@ impl AboutWindow {
             let info = format!(
                 "Version: {}\n\n\
                  Git SHA: {}\n\n\
+                 Target: {}\n\n\
                  Cargo profile: {}\n\n\
                  Built with rustc {}\n",
                 env!("VERGEN_GIT_SEMVER"),
                 env!("VERGEN_GIT_SHA"),
+                env!("VERGEN_CARGO_TARGET_TRIPLE"),
                 env!("VERGEN_CARGO_PROFILE"),
                 env!("VERGEN_RUSTC_SEMVER"),
             );
