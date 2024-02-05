6fdd255413c09f463f9a68d0475c66a80f9c1930
crumblingstatue <radiantstatue@gmail.com>
2024-02-06 00:52:53 +0100

    Preferences window: Make ui tabbed

--- src/gui/preferences_window.rs
+++ src/gui/preferences_window.rs
@@ -7,62 +7,100 @@ use {
 #[derive(Default)]
 pub struct PreferencesWindow {
     pub open: WindowOpen,
+    tab: Tab,
+}
+
+#[derive(Default, PartialEq)]
+enum Tab {
+    #[default]
+    Video,
+    Style,
+}
+
+impl Tab {
+    fn label(&self) -> &'static str {
+        match self {
+            Tab::Video => "Video",
+            Tab::Style => "Style",
+        }
+    }
 }
 
 impl PreferencesWindow {
-    pub fn ui(ui: &mut egui::Ui, _gui: &mut Gui, app: &mut App, rwin: &mut RenderWindow) {
-        ui.heading("Style");
-        let style = &mut app.cfg.style;
-        ui.heading("Font sizes");
-        let mut any_changed = false;
+    pub fn ui(ui: &mut egui::Ui, gui: &mut Gui, app: &mut App, rwin: &mut RenderWindow) {
         ui.horizontal(|ui| {
-            ui.label("heading");
-            any_changed |= ui
-                .add(egui::DragValue::new(&mut style.font_sizes.heading))
-                .changed();
+            ui.selectable_value(
+                &mut gui.preferences_window.tab,
+                Tab::Video,
+                Tab::Video.label(),
+            );
+            ui.selectable_value(
+                &mut gui.preferences_window.tab,
+                Tab::Style,
+                Tab::Style.label(),
+            );
         });
-        ui.horizontal(|ui| {
-            ui.label("body");
-            any_changed |= ui
-                .add(egui::DragValue::new(&mut style.font_sizes.body))
-                .changed();
-        });
-        ui.horizontal(|ui| {
-            ui.label("monospace");
-            any_changed |= ui
-                .add(egui::DragValue::new(&mut style.font_sizes.monospace))
-                .changed();
-        });
-        ui.horizontal(|ui| {
-            ui.label("button");
-            any_changed |= ui
-                .add(egui::DragValue::new(&mut style.font_sizes.button))
-                .changed();
-        });
-        ui.horizontal(|ui| {
-            ui.label("small");
-            any_changed |= ui
-                .add(egui::DragValue::new(&mut style.font_sizes.small))
-                .changed();
-        });
-        if ui.button("Reset default").clicked() {
-            *style = config::Style::default();
-            any_changed = true;
-        }
-        if any_changed {
-            crate::gui::set_font_sizes_ctx(ui.ctx(), style);
-        }
         ui.separator();
-        ui.heading("Video");
-        if ui.checkbox(&mut app.cfg.vsync, "Vsync").clicked() {
-            rwin.set_vertical_sync_enabled(app.cfg.vsync);
+        match gui.preferences_window.tab {
+            Tab::Video => video_ui(ui, app, rwin),
+            Tab::Style => style_ui(app, ui),
         }
-        ui.horizontal(|ui| {
-            ui.label("FPS limit (0 to disable)");
-            ui.add(egui::DragValue::new(&mut app.cfg.fps_limit));
-            if ui.button("Set").clicked() {
-                rwin.set_framerate_limit(app.cfg.fps_limit);
-            }
-        });
+    }
+}
+
+fn video_ui(ui: &mut egui::Ui, app: &mut App, rwin: &mut RenderWindow) {
+    ui.heading("Video");
+    if ui.checkbox(&mut app.cfg.vsync, "Vsync").clicked() {
+        rwin.set_vertical_sync_enabled(app.cfg.vsync);
+    }
+    ui.horizontal(|ui| {
+        ui.label("FPS limit (0 to disable)");
+        ui.add(egui::DragValue::new(&mut app.cfg.fps_limit));
+        if ui.button("Set").clicked() {
+            rwin.set_framerate_limit(app.cfg.fps_limit);
+        }
+    });
+}
+
+fn style_ui(app: &mut App, ui: &mut egui::Ui) {
+    let style = &mut app.cfg.style;
+    ui.heading("Font sizes");
+    let mut any_changed = false;
+    ui.horizontal(|ui| {
+        ui.label("heading");
+        any_changed |= ui
+            .add(egui::DragValue::new(&mut style.font_sizes.heading))
+            .changed();
+    });
+    ui.horizontal(|ui| {
+        ui.label("body");
+        any_changed |= ui
+            .add(egui::DragValue::new(&mut style.font_sizes.body))
+            .changed();
+    });
+    ui.horizontal(|ui| {
+        ui.label("monospace");
+        any_changed |= ui
+            .add(egui::DragValue::new(&mut style.font_sizes.monospace))
+            .changed();
+    });
+    ui.horizontal(|ui| {
+        ui.label("button");
+        any_changed |= ui
+            .add(egui::DragValue::new(&mut style.font_sizes.button))
+            .changed();
+    });
+    ui.horizontal(|ui| {
+        ui.label("small");
+        any_changed |= ui
+            .add(egui::DragValue::new(&mut style.font_sizes.small))
+            .changed();
+    });
+    if ui.button("Reset default").clicked() {
+        *style = config::Style::default();
+        any_changed = true;
+    }
+    if any_changed {
+        crate::gui::set_font_sizes_ctx(ui.ctx(), style);
     }
 }
