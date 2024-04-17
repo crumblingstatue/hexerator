9f7b9b1971943d25aa5e9b4acace1c7459a41518
crumblingstatue <radiantstatue@gmail.com>
2024-04-17 17:27:23 +0200

    bookmarks window: Focus and select text when clicking name edit button

--- src/gui/bookmarks_window.rs
+++ src/gui/bookmarks_window.rs
@@ -209,6 +209,9 @@ impl BookmarksWindow {
                 }
                 if ui.button("✏").clicked() {
                     win.edit_name ^= true;
+                    if win.edit_name {
+                        win.focus_text_edit = true;
+                    }
                 }
                 if ui.button("⮩").on_hover_text("Jump").clicked() {
                     action = Action::Goto(mark.offset);
