8f0e0ecc034572b541b500a3823a18ace588dfa3
crumblingstatue <radiantstatue@gmail.com>
2024-07-19 17:38:53 +0200

    Dep update

--- build.rs
+++ build.rs
@@ -1,14 +1,22 @@
-fn main() {
-    if let Err(e) = vergen::EmitBuilder::builder()
-        .git_sha(false)
-        .git_commit_timestamp()
-        .build_timestamp()
-        .cargo_target_triple()
-        .cargo_debug()
-        .cargo_opt_level()
-        .rustc_semver()
-        .emit()
-    {
-        println!("cargo:warning=Vergen failed with error: {e}");
-    }
+use {
+    std::error::Error,
+    vergen_gitcl::{BuildBuilder, CargoBuilder, Emitter, GitclBuilder, RustcBuilder},
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let gitcl = GitclBuilder::default().sha(false).commit_timestamp(true).build()?;
+    let build = BuildBuilder::default().build_timestamp(true).build()?;
+    let cargo = CargoBuilder::default()
+        .target_triple(true)
+        .debug(true)
+        .opt_level(true)
+        .build()?;
+    let rustc = RustcBuilder::default().semver(true).build()?;
+    Emitter::default()
+        .add_instructions(&gitcl)?
+        .add_instructions(&build)?
+        .add_instructions(&cargo)?
+        .add_instructions(&rustc)?
+        .emit()?;
+    Ok(())
 }
