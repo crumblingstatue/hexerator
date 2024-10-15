918c234cf05629495e68b86d1dd9cbca360e9bfb
crumblingstatue <radiantstatue@gmail.com>
2024-10-15 20:08:20 +0200

    x86_asm window: Don't auto shrink scroll area

--- src/gui/dialogs/x86_asm.rs
+++ src/gui/dialogs/x86_asm.rs
@@ -34,22 +34,25 @@ impl Dialog for X86AsmDialog {
         _line_spacing: u16,
     ) -> bool {
         let mut retain = true;
-        egui::ScrollArea::vertical().max_height(320.0).show(ui, |ui| {
-            egui::Grid::new("asm_grid").num_columns(2).show(ui, |ui| {
-                for instr in &self.decoded {
-                    let Some(sel_begin) = app.hex_ui.selection().map(|sel| sel.begin) else {
-                        ui.label("No selection");
-                        return;
-                    };
-                    let instr_off = instr.offset + sel_begin;
-                    if ui.link(instr_off.to_string()).clicked() {
-                        app.search_focus(instr_off);
+        egui::ScrollArea::vertical()
+            .auto_shrink(false)
+            .max_height(320.0)
+            .show(ui, |ui| {
+                egui::Grid::new("asm_grid").num_columns(2).show(ui, |ui| {
+                    for instr in &self.decoded {
+                        let Some(sel_begin) = app.hex_ui.selection().map(|sel| sel.begin) else {
+                            ui.label("No selection");
+                            return;
+                        };
+                        let instr_off = instr.offset + sel_begin;
+                        if ui.link(instr_off.to_string()).clicked() {
+                            app.search_focus(instr_off);
+                        }
+                        ui.label(&instr.string);
+                        ui.end_row();
                     }
-                    ui.label(&instr.string);
-                    ui.end_row();
-                }
+                });
             });
-        });
         ui.separator();
         match app.hex_ui.selection() {
             Some(sel) => {
