d59b1ce68d91715a6dfb2cf9c27ee978c41f1260
crumblingstatue <radiantstatue@gmail.com>
2025-03-15 17:13:19 +0100

    parse_hex_string: Remove weird unnecessary type annotation

--- src/find_util.rs
+++ src/find_util.rs
@@ -11,8 +11,5 @@ pub fn find_hex_string(
 }
 
 pub fn parse_hex_string(hex_string: &str) -> Result<Vec<u8>, std::num::ParseIntError> {
-    hex_string
-        .split_whitespace()
-        .map(|s| u8::from_str_radix(s, 16))
-        .collect::<Result<Vec<_>, _>>()
+    hex_string.split_whitespace().map(|s| u8::from_str_radix(s, 16)).collect()
 }
