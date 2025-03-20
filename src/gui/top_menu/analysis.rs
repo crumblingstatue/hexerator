0ed6296f0564fabeaed84cb19b2b014dd12be0df
crumblingstatue <radiantstatue@gmail.com>
2025-03-20 20:40:37 +0100

    top_menu/analysis: Add icons, small fixes

--- src/gui/top_menu/analysis.rs
+++ src/gui/top_menu/analysis.rs
@@ -1,11 +1,25 @@
-use crate::{
-    app::App,
-    gui::{Gui, message_dialog::Icon},
-    shell::msg_if_fail,
+use {
+    crate::{
+        app::App,
+        gui::{Gui, message_dialog::Icon},
+        shell::msg_if_fail,
+    },
+    constcat::concat,
+    egui_phosphor::regular as ic,
 };
 
+const L_DETERMINE_DATA_MIME: &str =
+    concat!(ic::SEAL_QUESTION, " Determine data mime type under cursor");
+const L_DETERMINE_DATA_MIME_SEL: &str =
+    concat!(ic::SEAL_QUESTION, " Determine data mime type of selection");
+const L_DIFF_WITH_FILE: &str = concat!(ic::GIT_DIFF, " Diff with file...");
+const L_DIFF_WITH_SOURCE_FILE: &str = concat!(ic::GIT_DIFF, " Diff with source file");
+const L_DIFF_WITH_BACKUP: &str = concat!(ic::GIT_DIFF, " Diff with backup");
+const L_FIND_MEMORY_POINTERS: &str = concat!(ic::ARROW_UP_RIGHT, " Find memory pointers...");
+const L_ZERO_PARTITION: &str = concat!(ic::BINARY, " Zero partition...");
+
 pub fn ui(ui: &mut egui::Ui, gui: &mut Gui, app: &App) {
-    if ui.button("Determine data mime type under cursor").clicked() {
+    if ui.button(L_DETERMINE_DATA_MIME).clicked() {
         gui.msg_dialog.open(
             Icon::Info,
             "Data mime type under cursor",
@@ -14,21 +28,21 @@ pub fn ui(ui: &mut egui::Ui, gui: &mut Gui, app: &App) {
         ui.close_menu();
     }
     if let Some(region) = app.hex_ui.selection() {
-        if ui.button("Determine data mime type of selection").clicked() {
+        if ui.button(L_DETERMINE_DATA_MIME_SEL).clicked() {
             gui.msg_dialog.open(
                 Icon::Info,
-                "Data mime type under cursor",
+                "Data mime type of selection",
                 tree_magic_mini::from_u8(&app.data[region.begin..=region.end]).to_string(),
             );
             ui.close_menu();
         }
     }
     ui.separator();
-    if ui.button("Diff with file...").clicked() {
+    if ui.button(L_DIFF_WITH_FILE).clicked() {
         gui.fileops.diff_with_file(app.source_file());
         ui.close_menu();
     }
-    if ui.button("Diff with source file").clicked() {
+    if ui.button(L_DIFF_WITH_SOURCE_FILE).clicked() {
         ui.close_menu();
         if let Some(path) = app.source_file() {
             let path = path.to_owned();
@@ -41,7 +55,7 @@ pub fn ui(ui: &mut egui::Ui, gui: &mut Gui, app: &App) {
     }
     match app.backup_path() {
         Some(path) if path.exists() => {
-            if ui.button("Diff with backup").clicked() {
+            if ui.button(L_DIFF_WITH_BACKUP).clicked() {
                 ui.close_menu();
                 msg_if_fail(
                     app.diff_with_file(path, &mut gui.win.file_diff_result),
@@ -51,22 +65,23 @@ pub fn ui(ui: &mut egui::Ui, gui: &mut Gui, app: &App) {
             }
         }
         _ => {
-            ui.add_enabled(false, egui::Button::new("Diff with backup"));
+            ui.add_enabled(false, egui::Button::new(L_DIFF_WITH_BACKUP));
         }
     }
     ui.separator();
     if ui
         .add_enabled(
             gui.win.open_process.selected_pid.is_some(),
-            egui::Button::new("Find memory pointers..."),
+            egui::Button::new(L_FIND_MEMORY_POINTERS),
         )
+        .on_disabled_hover_text("Requires open process")
         .clicked()
     {
         gui.win.find_memory_pointers.open.toggle();
         ui.close_menu();
     }
     if ui
-        .button("Zero partition...")
+        .button(L_ZERO_PARTITION)
         .on_hover_text("Find regions of non-zero data separated by zeroed regions")
         .clicked()
     {
