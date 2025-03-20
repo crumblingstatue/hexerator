c5ae526765ce8853a5aa66017902ac0a1c951090
crumblingstatue <radiantstatue@gmail.com>
2025-03-20 19:31:47 +0100

    top_menu/cursor: Add icons

--- src/gui/top_menu/cursor.rs
+++ src/gui/top_menu/cursor.rs
@@ -3,11 +3,18 @@ use {
         app::App,
         gui::{Gui, dialogs::JumpDialog},
     },
+    constcat::concat,
     egui::Button,
+    egui_phosphor::regular as ic,
 };
 
+const L_RESET: &str = concat!(ic::ARROW_U_UP_LEFT, " Reset");
+const L_JUMP: &str = concat!(ic::SHARE_FAT, " Jump...");
+const L_FLASH_CURSOR: &str = concat!(ic::LIGHTBULB, " Flash cursor");
+const L_CENTER_VIEW_ON_CURSOR: &str = concat!(ic::CROSSHAIR, " Center view on cursor");
+
 pub fn ui(ui: &mut egui::Ui, gui: &mut Gui, app: &mut App) {
-    let re = ui.button("Reset").on_hover_text(
+    let re = ui.button(L_RESET).on_hover_text(
         "Set to initial position.\n\
                         This will be --jump argument, if one was provided, 0 otherwise",
     );
@@ -15,16 +22,16 @@ pub fn ui(ui: &mut egui::Ui, gui: &mut Gui, app: &mut App) {
         app.set_cursor_init();
         ui.close_menu();
     }
-    if ui.add(Button::new("Jump...").shortcut_text("Ctrl+J")).clicked() {
+    if ui.add(Button::new(L_JUMP).shortcut_text("Ctrl+J")).clicked() {
         ui.close_menu();
         Gui::add_dialog(&mut gui.dialogs, JumpDialog::default());
     }
-    if ui.button("Flash cursor").clicked() {
+    if ui.button(L_FLASH_CURSOR).clicked() {
         app.preferences.hide_cursor = false;
         app.hex_ui.flash_cursor();
         ui.close_menu();
     }
-    if ui.button("Center view on cursor").clicked() {
+    if ui.button(L_CENTER_VIEW_ON_CURSOR).clicked() {
         app.preferences.hide_cursor = false;
         app.center_view_on_offset(app.edit_state.cursor);
         app.hex_ui.flash_cursor();
